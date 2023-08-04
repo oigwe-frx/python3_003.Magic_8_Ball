e986d464f01166cde44a4c93e769ef9f656ad6b1
Osita Igwe <93604075+oigwe-frx@users.noreply.github.com>
2023-08-04 12:29:03 -0400

    Update magic8.py

--- magic8.py
+++ magic8.py
@@ -1,8 +1,5 @@
 import random;
 
-# Name of the individual asking the questions
-name = "Osita";
-
 # Question Asked
 question = "Will I win the MegaMillion Lottery, tonight?";
 
@@ -10,7 +7,7 @@ question = "Will I win the MegaMillion Lottery, tonight?";
 answer = "";
 
 # Variable to store the randomly generated value and answer
-random_number = random.randint(1,9);
+random_number = random.randint(1,9)
 
 if random_number == 1:
   answer += "Yes - definitely"
@@ -30,13 +27,31 @@ elif random_number == 7:
   answer += "My sources say no"
 elif random_number == 8:
   answer += "Outlook not so good"
-elif randome_numer == 9:
+elif random_numer == 9:
   answer += "Very doubtful"
 else:
   answer += "Error"
 
 # Output of the question
-print(name + ' '  + "asks: " + question)
+# Name of the individual asking the questions
+
+#name = input('What is your name?')
+
+try:
+     name = input("What is your name?")
+     if name and question:
+       asks = "asks"
+       print(name + " asks: " + question)
+     elif name and not question:  
+       print(name + "The Magic 8-Ball cannot provide a fortune unless you ask it something.")
+     elif question:
+        print('Question: ' + question)
+     else: print("No name? No Question? Hmmm....")
+except EOFError:
+  print('Question: ' + question)
+
+
+
 
 # Output of the generated answer
 print("Magic 8-Ball's answer: " + answer)
